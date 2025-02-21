33d233acb619fe933c059e1aed551a429a0ef993
skerdudou <skerdudou@users.noreply.github.com>
2025-02-21 19:01:07 +0100

    [Llm] Refacto + request object in return (& facet handling)

--- vertigo-ai-langchain4j/src/main/java/io/vertigo/ai/llm/plugin/lc4j/Lc4jPlugin.java
+++ vertigo-ai-langchain4j/src/main/java/io/vertigo/ai/llm/plugin/lc4j/Lc4jPlugin.java
@@ -17,16 +17,25 @@
  */
 package io.vertigo.ai.llm.plugin.lc4j;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 import javax.inject.Inject;
 
 import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.output.Response;
 import dev.langchain4j.service.AiServices;
 import dev.langchain4j.service.Result;
+import dev.langchain4j.service.output.ServiceOutputParser;
 import io.vertigo.ai.llm.LlmPlugin;
 import io.vertigo.ai.llm.model.LlmChat;
 import io.vertigo.ai.llm.model.VLlmResult;
@@ -36,8 +45,10 @@ import io.vertigo.ai.llm.plugin.lc4j.document.Lc4jDocumentUtil;
 import io.vertigo.ai.llm.plugin.lc4j.document.VFileDocumentLoader;
 import io.vertigo.core.lang.Assertion;
 import io.vertigo.core.lang.VSystemException;
+import io.vertigo.core.node.Node;
 import io.vertigo.core.param.ParamValue;
 import io.vertigo.datastore.filestore.model.VFile;
+import io.vertigo.vega.engines.webservice.json.JsonEngine;
 
 /**
  * Defines llm plugin using OpenAi with langchain4j.
@@ -47,6 +58,9 @@ import io.vertigo.datastore.filestore.model.VFile;
 public final class Lc4jPlugin implements LlmPlugin {
 
 	private final ChatLanguageModel chatModel;
+	//private final StreamingChatLanguageModel chatModelStream;
+
+	private final ServiceOutputParser serviceOutputParser = new VServiceOutputParser();
 
 	@Inject
 	public Lc4jPlugin(
@@ -56,15 +70,23 @@ public final class Lc4jPlugin implements LlmPlugin {
 				.isNotBlank(apiKey); // langchain4j can use "demo" api key with a 5000 tokens limit (with langchain4j server acting as a proxy)
 		//---
 		chatModel = OpenAiChatModel.builder()
+				//.baseUrl("http://albert.dev.klee.lan.net:8000/v1")
 				.apiKey(apiKey)
+				//.modelName("turbo")
 				.modelName("gpt-4o-mini")
 				.temperature(0d)
 				.build();
 
+		//		chatModelStream = OpenAiStreamingChatModel.builder()
+		//				.apiKey(apiKey)
+		//				.modelName("gpt-4o-mini")
+		//				.temperature(0d)
+		//				.build();
+
 	}
 
 	@Override
-	public VLlmResult promptOnFiles(final VPrompt prompt, final Stream<VFile> files) {
+	public VLlmResult askOnFiles(final VPrompt prompt, final Stream<VFile> files) {
 		final List<Document> documents = files
 				.map(VFileDocumentLoader::loadDocument)
 				.toList();
@@ -92,8 +114,67 @@ public final class Lc4jPlugin implements LlmPlugin {
 	}
 
 	@Override
+	public <T> T ask(final VPrompt prompt, final Class<T> clazz) {
+		final var chatMessages = new ArrayList<ChatMessage>();
+		final var context = prompt.getContext();
+
+		if (context.getPersona() != null) {
+			chatMessages.add(Lc4jUtils.getSystemMessageFromPersona(context.getPersona()));
+		}
+
+		// append additional instructions as in dev.langchain4j.service.DefaultAiServices
+		// don't know why it is not done in the system message
+		final var additionalInstructions = serviceOutputParser.outputFormatInstructions(clazz);
+		chatMessages.add(UserMessage.from(prompt.getInstructions() + "\n" + additionalInstructions));
+
+		final var llmResponse = chatModel.generate(chatMessages);
+		return (T) serviceOutputParser.parse(llmResponse, clazz);
+	}
+
+	@Override
 	public LlmChat newChat(final Stream<VFile> files, final VPromptContext context) {
 		return new Lc4jChat(files.toList(), chatModel, context);
 	}
 
+	public static class VServiceOutputParser extends ServiceOutputParser {
+		private static final Pattern JSON_BLOCK_PATTERN = Pattern.compile("(?s)\\{.*\\}|\\[.*\\]"); // from serviceOutputParser
+
+		@Override
+		public String outputFormatInstructions(final Type returnType) {
+			if ("io.vertigo.ai.impl.llm.FacetPromptUtil$FacetPromptResult".equals(returnType.getTypeName())) {
+				return """
+						You must answer strictly in the following JSON format: {
+						"selectedFacetValues": {
+						  "__facetDefinitionId__": ["__facetValueCode__", "__facetValueCode2__", ...],
+						  "__facetDefinitionId2__": ["__facetValueCode3__"],
+						  ...
+						}(type: java.util.Map<String, List<String>>),
+						"criteria": (type: string)
+						}
+						""";
+			}
+			return super.outputFormatInstructions(returnType);
+		}
+
+		@Override
+		public Object parse(final Response<AiMessage> response, final Type returnType) {
+			final var jsonEngine = Node.getNode().getComponentSpace().resolve(JsonEngine.class);
+			final var text = response.content().text();
+			try {
+				return jsonEngine.fromJson(text, returnType);
+			} catch (final Exception e) {
+				final String jsonBlock = extractJsonBlock(text);
+				return jsonEngine.fromJson(jsonBlock, returnType);
+			}
+		}
+
+		private String extractJsonBlock(final String text) {
+			final Matcher matcher = JSON_BLOCK_PATTERN.matcher(text);
+			if (matcher.find()) {
+				return matcher.group();
+			}
+			return text;
+		}
+
+	}
 }
--- vertigo-ai-langchain4j/src/main/java/io/vertigo/ai/llm/plugin/lc4j/Lc4jTools.java
+++ vertigo-ai-langchain4j/src/main/java/io/vertigo/ai/llm/plugin/lc4j/Lc4jTools.java
@@ -1,12 +1,13 @@
 package io.vertigo.ai.llm.plugin.lc4j;
 
 import java.math.BigDecimal;
-import java.util.function.Function;
 
 import dev.langchain4j.agent.tool.P;
 import dev.langchain4j.agent.tool.Tool;
 import io.vertigo.commons.peg.PegNoMatchFoundException;
+import io.vertigo.commons.peg.PegParsingValueException;
 import io.vertigo.commons.peg.PegSolver;
+import io.vertigo.commons.peg.PegSolver.PegSolverFunction;
 import io.vertigo.commons.peg.rule.PegRule;
 import io.vertigo.commons.peg.rule.PegRules;
 import io.vertigo.commons.peg.rule.PegWordRuleMode;
@@ -16,10 +17,10 @@ public class Lc4jTools {
 
 	private static final PegRule<PegSolver<String, Object, Object>> RULE = PegRules.delayedOperation(PegRules.word(false, "-0123456789.", PegWordRuleMode.ACCEPT, "0-9"),
 			PegArithmeticsOperatorTerm.class, false);
-	private static final Function<String, Object> TERM_PARSER = BigDecimal::new;
+	private static final PegSolverFunction<String, Object> TERM_PARSER = BigDecimal::new;
 
 	@Tool("Returns the computation of the mathematical expression. At least 1 operation is mandatory in the expression. It handles addition, substraction, multiplication and division. It does handle parenthesis. It can be used to compute amount, tax values, etc. Example, you can compute '2 + 2' or '(2 + 2) * 2'.You can also substract 20% taxes with '100 / 1.2', but be careful to round afterward. ")
-	public BigDecimal compute(@P("The mathematical expression to compute") final String expression) throws PegNoMatchFoundException {
+	public BigDecimal compute(@P("The mathematical expression to compute") final String expression) throws PegNoMatchFoundException, PegParsingValueException {
 		return evaluate(expression);
 	}
 
@@ -28,11 +29,11 @@ public class Lc4jTools {
 	 */
 	@Tool("Returns the tax from an amount including taxes.")
 	public BigDecimal computeTax(@P("The amount including taxes") final BigDecimal amount, @P("The percentage of taxes, eg 0.20 for 20%") final BigDecimal taxPercentage)
-			throws PegNoMatchFoundException {
+			throws PegNoMatchFoundException, PegParsingValueException {
 		return evaluate("(" + amount + " * " + taxPercentage + ") / (1 + " + taxPercentage + ")");
 	}
 
-	private static BigDecimal evaluate(final String expression) throws PegNoMatchFoundException {
+	private static BigDecimal evaluate(final String expression) throws PegNoMatchFoundException, PegParsingValueException {
 		return (BigDecimal) RULE.parse(expression).getValue().apply(TERM_PARSER);
 	}
 
